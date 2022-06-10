f8d0eda5a3a6660408338206fb9c190a5c39037f
Matthieu Zhang <jd4625988@gmail.com>
2022-06-10 16:08:25 +0200

    new version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.4.0"
+      version = "4.18.0"
     }
   }
 }
@@ -25,17 +25,6 @@ module "storage" {
   default_tags   = var.default_tags
   db_username    = var.db_username
   db_password    = var.db_password
-}
-
-module "compute" {
-  source = "./modules/compute"
-  environment = var.environment
-  region = var.region
-  elb_target_group_arn = module.network.target_lb_group_arn
-  private_subnet = module.network.private_subnet
-  public_subnet = module.network.public_subnet
-  elb_sg = module.network.target_lb_security_group
-  elb_url = module.network.elb_url
 }
 
 resource "local_file" "web-access" {
